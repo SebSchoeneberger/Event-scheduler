92c8c1479b20cc0088416c0a15a852741b1168db
Sebastian Schoeneberger <sebastian.schoeneberger@gmail.com>
2024-07-01 22:39:57 +0200

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+import { useState } from 'react'
+import {
+  createBrowserRouter,
+  createRoutesFromElements,
+  Link,
+  Outlet,
+  Route,
+  RouterProvider,
+} from 'react-router-dom';
+import './App.css'
+
+function App() {
+
+
+  return (
+    <>
+      
+    </>
+  )
+}
+
+export default App
